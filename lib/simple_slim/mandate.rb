93662563534cd30ec111cc4870abdf353087a1f9
David FRANCOIS <david.francois@paymium.com>
2017-05-19 16:10:53 +0400

    Ability to retrieve a mandate given its RUM

--- lib/simple_slim/mandate.rb
+++ lib/simple_slim/mandate.rb
@@ -5,6 +5,12 @@ module SimpleSlim
   #
   class Mandate
 
+    attr_accessor :rum, :state, :signature_order, :iban, :bic, :bank_name
+
+    def initialize
+      yield(self) if block_given?
+    end
+
     #
     # Creates a new mandate signature request. This request is then submitted to
     # the customer who can then sign it either on Slimpay's interface, or in an
@@ -13,7 +19,7 @@ module SimpleSlim
     # The +subscriber_reference+ attribute must be unique to a subscriber and
     # would usually be a user ID.
     #
-    # All parameters are mandatory except +country+ and locale that default
+    # All parameters are mandatory except +country+ and +locale+ that default
     # to 'FR', and +address_2+ that defaults to +nil+.
     #
     # The method returns a hash containing both the URL to redirect the customer
@@ -35,7 +41,7 @@ module SimpleSlim
     # @return [Hash]
     #
     def self.create_signature_order(first_name:, family_name:, title:,
-                                    telephone:, 
+                                    telephone:,
                                     email:, address_1:, address_2: nil, city:,
                                     zip_code:, subscriber_reference:,
                                     country: 'FR', locale: 'fr')
@@ -76,29 +82,57 @@ module SimpleSlim
       url = res['_links']['https://api.slimpay.net/alps#user-approval']['href']
 
       # Fetch approval iframe
-      iframe = JSON.load(order.extended_user_approval(mode: 'iframeembedded').parsed_response)['content']
+      # iframe = JSON.load(order.extended_user_approval(mode: 'iframeembedded').parsed_response)['content']
+      iframe = JSON.load(order.extended_user_approval(mode: 'iframepopin').parsed_response)['content']
 
       { success: true, iframe: Base64.decode64(iframe), url: url }
     end
 
     #
-    # Retrieves a mandate given a reference
+    # Retrieves a mandate given a the reference of a signature order
     #
-    # @param reference [String]
+    # @param reference [String] Order signature reference
     # @return [SimpleSlim::Mandate]
     #
-    def self.find(reference)
-      raise 'implement me'
+    def self.get_from_signature_order(reference)
+      order = Slimpay::Order.new
+      order.get_one(reference)
+      mandate = JSON.load(order.get_mandate.parsed_response)
+      bank_account = JSON.load(order.get_bank_account)
+      build_from(mandate, bank_account)
     end
 
     #
-    # Charges an EUR amount against this mandate
+    # Retrieves a mandate given a RUM (reference unique de mandat)
     #
-    # @param amount [Integer] Amount to charge in EUR cents
-    # @return charge [SimpleSlim::Charge]
+    # @param rum [String]
+    # @return [SimpleSlim::Mandate]
     #
-    def charge
-      raise 'implement me'
+    def self.get_from_rum(rum)
+      mand = Slimpay::Mandate.new
+      mandate = JSON.load(mand.get_one(rum).parsed_response)
+      bank_account = JSON.load(mand.get_bank_account)
+      build_from(mandate, bank_account)
+    end
+
+
+    protected
+
+    #
+    # Instantiates a +SimpleSlim::Mandate+ given Slimpay's representations
+    #
+    # @param mandate [Hash]
+    # @param bank_account [Hash]
+    # @return [SimpleSlim::Mandate]
+    #
+    def self.build_from(mandate, bank_account)
+      SimpleSlim::Mandate.new do |m|
+        m.rum             = mandate['rum']
+        m.state           = mandate['state']
+        m.iban            = bank_account['iban']
+        m.bic             = bank_account['bic']
+        m.bank_name       = bank_account['institutionName']
+      end
     end
 
   end
--- spec/simple_slim/mandate_spec.rb
+++ spec/simple_slim/mandate_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../spec_helper'
 
-RSpec.describe SimpleSlim::Mandate do
+RSpec.describe SimpleSlim::Mandate, :vcr do
 
   let(:correct_mandate_data) do
     {
@@ -22,7 +22,7 @@ RSpec.describe SimpleSlim::Mandate do
     correct_mandate_data
   end
 
-  describe '.create_signature_order', :vcr do
+  describe '.create_signature_order' do
     it 'should successfully create a signature order with correct params' do
       res = SimpleSlim::Mandate.create_signature_order(**correct_mandate_data)
 
@@ -37,12 +37,32 @@ RSpec.describe SimpleSlim::Mandate do
     end
   end
 
-  describe '.find' do
-    pending
+  describe '.get_from_signature_order' do
+    it 'should return a SimpleSlim::Mandate instance' do
+      reference = '84ca416e-3c7f-11e7-9800-000000000000'
+      mandate = SimpleSlim::Mandate.get_from_signature_order(reference)
+
+      expect(mandate).to be_an_instance_of(SimpleSlim::Mandate)
+      expect(mandate.rum).to eql('SLMP003441095')
+      expect(mandate.state).to eql('active')
+      expect(mandate.iban).to eql('FR7616348000019212905486870')
+      expect(mandate.bic).to eql('SLMPFRP1XXX')
+      expect(mandate.bank_name).to eql('SLIMPAY')
+    end
   end
 
-  describe '#charge' do
-    pending
+  describe '.get_from_rum' do
+    it 'should return the correct mandate as a SimpleSlim::Mandate' do
+      rum = 'SLMP003441095'
+      mandate = SimpleSlim::Mandate.get_from_rum(rum)
+
+      expect(mandate).to be_an_instance_of(SimpleSlim::Mandate)
+      expect(mandate.rum).to eql(rum)
+      expect(mandate.state).to eql('active')
+      expect(mandate.iban).to eql('FR7616348000019212905486870')
+      expect(mandate.bic).to eql('SLMPFRP1XXX')
+      expect(mandate.bank_name).to eql('SLIMPAY')
+    end
   end
 
 end
